4c485951f3ae7320bc194f29f139d57bc63d4f8d
Thang <lhuthng@gmail.com>
2022-02-17 21:11:12 +0100

    port 3000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server  = require('http').createServer(app);
-const port = process.env.port || 80;
+const port = process.env.port || 3000;
 
 server.listen(port, () => {
   console.log(port);
